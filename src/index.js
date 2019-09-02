51b1af7a076df076ae5c6ea46276f0f43b7c9e88
Oskar Schöldström <m@oxy.fi>
2019-09-02 11:59:30 -0300

    align with how core template locks work

--- src/index.js
+++ src/index.js
@@ -8,52 +8,100 @@ const SYNCHRONIZE_TEMPLATE_NOTICE_ID = 'SYNCHRONIZE_TEMPLATE_NOTICE_ID';
 class GutenbergTemplates {
   constructor() {
     this.previousTemplate = null;
-    this.template = null;
-    this.initialLoad = true;
+    this.template = undefined;
 
-    subscribe(() => {
-      this.initialLoad = false;
-      this.subscribe();
-    });
-    // Trigger initial load
-    this.subscribe();
+    // Subscribe to changes
+    subscribe(this.subscribe.bind(this));
   }
 
   subscribe() {
     const newTemplate = select('core/editor').getEditedPostAttribute('template');
+    // Not known yet
+    if (newTemplate === undefined) {
+      console.log('template not known');
+      return;
+    }
 
+    // This is the initial template on editor load
+    if (this.template === undefined) {
+      console.log('initial template');
+      this.template = newTemplate;
+      if (this.template) {
+        this.setInitialTemplate(this.template);
+      }
+      return;
+    }
+
+    // The template has changed
     if (newTemplate !== this.template) {
+      console.log('template has changed');
       this.previousTemplate = this.template;
       this.template = newTemplate;
 
+      // An actual template was set
       if (newTemplate) {
-        this.changeTemplate();
-      } else if (this.previousTemplate !== null) {
-        // If we're going back to default template.
-        dispatch('core/editor').updateEditorSettings({templateLock: false});
+        this.changeTemplate(this.template);
+        return;
+      }
+
+      // We're setting the Default template.
+      if (newTemplate === '') {
+        console.log('setting default template')
+        const { updateSettings, setTemplateValidity } = dispatch('core/block-editor');
+
+        updateSettings({templateLock: false});
+        setTemplateValidity(true);
       }
     }
   }
 
-  changeTemplate() {
-    const { resetBlocks, editPost, updateEditorSettings } = dispatch('core/editor');
-    const { createWarningNotice, removeNotice } = dispatch('core/notices');
+  setInitialTemplate(templateName) {
+    const { updateSettings, setTemplateValidity } = dispatch('core/block-editor');
 
-    apiRequest({ path: '/gutenberg-templates/v1/template', data: {template: this.template} }).then(config => {
-      const currentBlocks = select('core/editor').getBlocks();
+    apiRequest({ path: '/gutenberg-templates/v1/template', data: {template: templateName} }).then(config => {
+      const currentBlocks = select('core/block-editor').getBlocks();
       const template = config.template;
       const templateLock = config.template_lock;
-      const isValidTemplate = !currentBlocks.length || doBlocksMatchTemplate(currentBlocks, template);
+      const isBlocksValidToTemplate = (
+        !template ||
+        templateLock !== 'all' ||
+        doBlocksMatchTemplate(currentBlocks, template)
+      );
+
+      updateSettings({
+        templateLock,
+        template
+      });
+      setTemplateValidity(isBlocksValidToTemplate);
+    });
+  }
+
+  changeTemplate(templateName) {
+    console.log('change template');
+    const { resetBlocks, updateSettings } = dispatch('core/block-editor');
+    const { createWarningNotice, removeNotice } = dispatch('core/notices');
+
+    apiRequest({ path: '/gutenberg-templates/v1/template', data: {template: templateName} }).then(config => {
+      const currentBlocks = select('core/block-editor').getBlocks();
+      const template = config.template;
+      const templateLock = config.template_lock;
+
+      const isBlocksValidToTemplate = (
+        !template ||
+        doBlocksMatchTemplate(currentBlocks, template)
+      );
 
       const synchronizeTemplate = () => {
+        console.log(currentBlocks, template);
         resetBlocks(synchronizeBlocksWithTemplate(currentBlocks, template));
-        updateEditorSettings({ templateLock });
+        updateSettings({ template, templateLock });
       };
 
       const denySynchronization = () => {
+        console.log('revert');
         // If it's a locked template, revert the setting.
         if (templateLock === 'all') {
-          editPost({ template: this.previousTemplate });
+          this.revertTemplate();
         }
         removeNotice(SYNCHRONIZE_TEMPLATE_NOTICE_ID);
       };
@@ -65,26 +113,28 @@ class GutenbergTemplates {
         removeNotice(SYNCHRONIZE_TEMPLATE_NOTICE_ID);
       };
 
-      if (isValidTemplate) {
+      // The template is valid, just synchronize it and set the lock.
+      if (isBlocksValidToTemplate) {
         synchronizeTemplate();
-      } else if (this.wasDefaultTemplate()) {
-        createWarningNotice(
-          __('The content of your post doesn\'t match the assigned template.', 'wp-gutenberg-templates'),
-          {
-            actions: [
-              { label: __('Keep it as is', 'wp-gutenberg-templates'), onClick: denySynchronization, className: 'is-link' },
-              { label: __('Reset the template', 'wp-gutenberg-templates'), onClick: confirmSynchronization, className: 'is-link' },
-            ],
-            isDismissible: false,
-            id: SYNCHRONIZE_TEMPLATE_NOTICE_ID,
-          }
-        );
       }
+
+      // Template is not valid, confirm what to do
+      createWarningNotice(
+        __('The content of your post doesn\'t match the assigned template.', 'wp-gutenberg-templates'),
+        {
+          actions: [
+            { label: __('Keep it as is', 'wp-gutenberg-templates'), onClick: denySynchronization },
+            { label: __('Reset the template', 'wp-gutenberg-templates'), onClick: confirmSynchronization, className: 'is-primary' },
+          ],
+          isDismissible: true, // to make it sticky, dismissing doesnt trigger deny though.
+          id: SYNCHRONIZE_TEMPLATE_NOTICE_ID,
+        }
+      );
     });
   }
 
-  wasDefaultTemplate() {
-    return this.previousTemplate === '' && !this.initialLoad;
+  revertTemplate() {
+    dispatch('core/editor').editPost({ template: this.previousTemplate });
   }
 }
 
